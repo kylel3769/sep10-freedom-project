215b328c74dc474425e42c98e9c34b83a63f71a7
Kyle Lam <kylel220811@gmail.com>
2024-05-01 21:07:11 -0400

    fixing website attempt 1

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
+/*!
+* Start Bootstrap - Simple Sidebar v6.0.6 (https://startbootstrap.com/template/simple-sidebar)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
+*/
+//
+// Scripts
+//
+
 window.addEventListener('DOMContentLoaded', event => {
 
-        // Toggle the side navigation
+    // Toggle the side navigation
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
     if (sidebarToggle) {
         // Uncomment Below to persist sidebar toggle between refreshes
